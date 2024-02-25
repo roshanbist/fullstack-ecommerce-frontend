02114e97f3bd08eb012b2b5377a4c1aea23a209c
roshan bist <roshan.bist@tuni.fi>
2024-02-25 20:33:25 +0200

    fix navigation issue

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -54,39 +54,49 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='md:flex-1 md:mx-10' ref={navRef}>
-      <span className='md:hidden cursor-pointer text-[20px] relative z-10'>
-        {isMobile ? (
-          <FontAwesomeIcon icon={faXmark} onClick={() => setIsMobile(false)} />
-        ) : (
-          <FontAwesomeIcon icon={faBars} onClick={mobileMenuHandler} />
-        )}
-      </span>
-      <div
-        className={`max-md:fixed w-full left-0 top-0 max-md:pt-[67px] bottom-0 max-md:transition-all max-md:ease-in-out max-md:duration-300 ${
-          isMobile ? 'translate-x-0' : 'max-md:translate-x-full'
-        } max-md:bg-light-white max-md:border-t max-md:border-t-neutral-300`}
-      >
-        <ul className='md:flex md:gap-10 font-medium md:text-[18px]'>
-          {navLinks.map((navItem) => (
-            <li
-              key={navItem.label}
-              className='max-md:px-5 max-md:py-4 max-md:shadow-xl'
-            >
-              <Link
-                className={`hover:text-blue-600 font-medium transition-colors ease-in-out duration-300 ${
-                  location.pathname === navItem.href ? 'text-blue-600' : ''
-                }`}
-                to={navItem.href}
-                onClick={navCloseHandler}
+    <div className='flex flex-1 max-md:justify-end'>
+      <nav className='md:flex-1 md:mx-10' ref={navRef}>
+        <span className='md:hidden cursor-pointer text-[24px] leading-[1] relative z-10'>
+          {isMobile ? (
+            <FontAwesomeIcon
+              className='text-blue-500'
+              icon={faXmark}
+              onClick={() => setIsMobile(false)}
+            />
+          ) : (
+            <FontAwesomeIcon
+              className='text-blue-500'
+              icon={faBars}
+              onClick={mobileMenuHandler}
+            />
+          )}
+        </span>
+        <div
+          className={`max-md:fixed w-full left-0 max-md:top-[65px] bottom-0 max-md:transition-all max-md:ease-in-out max-md:duration-300 ${
+            isMobile ? 'translate-x-0' : 'max-md:translate-x-full'
+          } max-md:bg-palette-primary max-md:border-t max-md:border-t-palette-accent`}
+        >
+          <ul className='md:flex md:gap-10 font-medium md:text-[18px]'>
+            {navLinks.map((navItem) => (
+              <li
+                key={navItem.label}
+                className='max-md:px-5 max-md:py-4 max-md:border-b max-md:border-b-palette-accent'
               >
-                {navItem.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+                <Link
+                  className={`hover:text-blue-600 font-medium transition-colors ease-in-out duration-300 ${
+                    location.pathname === navItem.href ? 'text-blue-600' : ''
+                  }`}
+                  to={navItem.href}
+                  onClick={navCloseHandler}
+                >
+                  {navItem.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
