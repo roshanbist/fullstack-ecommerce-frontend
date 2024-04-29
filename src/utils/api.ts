07ad3b3c036d4781bebfdec725e2bdda835e71bc
roshan bist <roshan.bist@tuni.fi>
2024-04-29 10:56:55 +0300

    replaced the backend deploy link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080/api/v1';
+// export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL =
+  'https://fullstack-ecommerce-backend-sx23.onrender.com/api/v1';
